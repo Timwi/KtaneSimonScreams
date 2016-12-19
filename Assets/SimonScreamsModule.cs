e399c5b283cf36cdb3aa97836843f4bcfbcbb59e
Timwi <github@timwi.de>
2016-12-19 12:13:25 +0100

    First commit

---
+++ Assets/SimonScreamsModule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SimonScreams;
+using UnityEngine;
+using Rnd = UnityEngine.Random;
+
+/// <summary>
+/// On the Subject of Simon Screams
+/// Created by Timwi
+/// </summary>
+public class SimonScreamsModule : MonoBehaviour
+{
+    public KMBombInfo Bomb;
+    public KMBombModule Module;
+    public KMAudio Audio;
+
+    public KMSelectable[] Buttons;
+
+    void Start()
+    {
+        Debug.Log("[Simon Screams] Started");
+    }
+
+    void ActivateModule()
+    {
+        Debug.Log("[Simon Screams] Activated");
+    }
+}
